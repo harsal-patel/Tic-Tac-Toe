baffd714fa452e2eebddaab41b624bc3896be8d9
harsal-patel <78583720+harsal-patel@users.noreply.github.com>
2021-03-08 08:47:50 -0800

    Update tic tac toe.cpp

--- tic tac toe.cpp
+++ tic tac toe.cpp
@@ -15,8 +15,8 @@ int main() {
 							{'7', '8', '9'}
 						};
 	
-	cout << "\t   TIC - TAC - TOE" << endl;
-	cout << "\n   Player 1 [X]       Player 2 [O]" << endl;
+	cout << "\t   TIC - TAC - TOE" << endl;					//Shows board at the start of the game before first person makes a move.
+	cout << "\n   Player 1 [X]       Player 2 [O]" << endl;			//Board uses numbers 1 - 9 as spots that a player can select.
 	cout << endl;
 	cout << "\t      |       |" << endl;
 	cout << "\t  " << board[0][0] << "   |   " << board[0][1] << "   |   " << board [0][2] << endl;
@@ -28,20 +28,20 @@ int main() {
 	cout << "\t  " << board[2][0] << "   |   " << board[2][1] << "   |   " << board [2][2] << endl;
 	cout << "\t      |       |" << endl;
 	
-	while (!isWinner) {
-		int player = (active == 'X') ? 1 : 2;
+	while (!isWinner) {							//Loops until isWinner is assigned "true".
+		int player = (active == 'X') ? 1 : 2;				//Player is assigned to 1 or 2 depending on if 'X' or 'O' is the active turn.
 		cout << "\n    Player " << player << ", pick a spot: ";
 		getline(cin, checkInput);
-		if (checkInput.size() > 1 || !isdigit(checkInput.at(0))) {
+		if (checkInput.size() > 1 || !isdigit(checkInput.at(0))) {	//Prevents user from entering multiple numbers/letters that would throw off the switch statement.
 			cout << "\n    Invalid input, try again." << endl;
 			continue;
 		}
 		else {
 			userInput = checkInput.at(0);
 		}
-		switch(userInput) {
+		switch(userInput) {						//Matches userInput with spot on board. If the spot is taken already, prompts user to try another input.
 		case '1':
-			if (board[0][0] == userInput) {
+			if (board[0][0] == userInput) {				//Checks if spot is taken by comparing the character at spot with userInput. If they don't match, the spot is taken.
 				board[0][0] = active;
 			}
 			else {
@@ -121,14 +121,14 @@ int main() {
 				continue;
 			}
 			break;
-		default:
+		default:							//If userInput isn't 1 - 9, goes to default and prompts user to input another choice.
 			cin.clear();
 			cin.ignore(10000, '\n');
 			cout << "\n    Invalid input, try again." << endl;
 			continue;
 	}
-		system("cls");
-		cout << "\t   TIC - TAC - TOE" << endl;
+		system("cls");							//Clears screen after every correct input.
+		cout << "\t   TIC - TAC - TOE" << endl;				//Shows updated board each time a correct input is entered.
 	cout << "\n   Player 1 <X>        Player 2 <O>" << endl;
 	cout << endl;
 	cout << "\t      |       |" << endl;
@@ -141,7 +141,7 @@ int main() {
 	cout << "\t  " << board[2][0] << "   |   " << board[2][1] << "   |   " << board [2][2] << endl;
 	cout << "\t      |       |" << endl;
 		
-		for (int i = 0; i < 3; i++) {
+		for (int i = 0; i < 3; i++) {					//For loop checks if there is a winner by checking straight lines through columns and rows.
 			if (board[i][0] == active && board[i][1] == active && board[i][2] == active) {
 			isWinner = true;
 			winner = active;
@@ -152,7 +152,7 @@ int main() {
 			winner = active;
 			break;
 			}
-		}
+		}								//Two if statements check diagonals for a winner.
 		if (board[0][0] == active && board[1][1] == active && board[2][2] == active) {
 			isWinner = true;
 			winner = active;
@@ -162,21 +162,21 @@ int main() {
 			winner = active;
 		}
 		
-		numTurns++;
-		if (numTurns >= 9) {
+		numTurns++;							//Maximum number of turns for a game is 9, so numTurns checks for a draw. If numTurns is reached, game is over.
+		if (numTurns >= 9) {						//When numTurns is reached, loop is broken without changing isWinner, so it remains "false".
 			break;
 		}
-		if (isWinner == false){	
+		if (isWinner == false){						//If there isn't a winner, the active character switches from 'X' to 'O' and vice versa.
 			active = (active == 'X') ? 'O' : 'X';
 		}
-	}
-	if (active == 'X' && isWinner) {
+	}									//End of game loop.
+	if (active == 'X' && isWinner) {					//If 'X' was the last move and isWinner is "true", Player 1 must be the winner.
 		cout << "\n\t   Player 1 Wins!" << endl;
 	}
-	else if (active == 'O' && isWinner){
+	else if (active == 'O' && isWinner){					//If 'O' was the last move and isWinner is "true", Player 2 must be the winner.
 		cout << "\n\t   Player 2 Wins!" << endl;
 	}
-	else {
+	else {									//Otherwise, the loop must have been broken by numTurns, so it must be a tie game.
 		cout << "\n\t      Tie Game!" << endl;
 	}
 	_getch();
